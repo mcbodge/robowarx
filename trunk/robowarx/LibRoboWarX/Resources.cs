2db46d8b31cbc88979691489e307399e93bbec32
stephan@kochen.nl <stephan@kochen.nl@4cb8c14c-e4c9-11dd-9059-71fac19e68c6>
2009-01-31 16:49:40 +0000

    Slightly neater resource handling.

--- trunk/robowarx/LibRoboWarX/Resources.cs
+++ trunk/robowarx/LibRoboWarX/Resources.cs
@@ -5,12 +5,60 @@ using System.Drawing;
 
 namespace RoboWarX
 {
-    public class Resources
+    public static class Resources
     {
-        public static Image GetImage(string name)
+        private static void LoadImages(Type container)
         {
             Assembly a = Assembly.GetExecutingAssembly();
-            return Image.FromStream(a.GetManifestResourceStream(name));
+            foreach (FieldInfo f in container.GetFields())
+            {
+                Console.WriteLine(String.Format("Checking {0}.{1}", container.Name, f.Name));
+                if (f.FieldType != typeof(Image)) continue;
+                f.SetValue(null, Image.FromStream(a.GetManifestResourceStream(
+                        String.Format("{0}.{1}", container.Name, f.Name)))); 
+            }
+        }
+        
+        public static class Projectile
+        {
+            public static Image ArmedMine;
+            public static Image Bullet;
+            public static Image DroneDown;
+            public static Image DroneDownLeft;
+            public static Image DroneDownRight;
+            public static Image DroneLeft;
+            public static Image DroneRight;
+            public static Image DroneUp;
+            public static Image DroneUpLeft;
+            public static Image DroneUpRight;
+            public static Image Hellbore;
+            public static Image NewMine;
+            
+            static Projectile()
+            {
+                LoadImages(typeof(Projectile));
+            }
+        }
+        
+        public static class Robot
+        {
+            public static Image Basic1;
+            public static Image Basic1Shield;
+            public static Image Basic2;
+            public static Image Basic2Shield;
+            public static Image Basic3;
+            public static Image Basic3Shield;
+            public static Image Basic4;
+            public static Image Basic4Shield;
+            public static Image Basic5;
+            public static Image Basic5Shield;
+            public static Image Basic6;
+            public static Image Basic6Shield;
+            
+            static Robot()
+            {
+                LoadImages(typeof(Robot));
+            }
         }
     }
 }
