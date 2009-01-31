78aa23950fafac0a6a85f1438a7a7a9155900e39
stephan <stephan@kochen.nl>
2009-01-31 17:41:06 +0000

    Commence hellbore firing!

--- robowarx/LibRoboWarX/Weapons/Hellbore.cs
+++ robowarx/LibRoboWarX/Weapons/Hellbore.cs
@@ -1,15 +1,129 @@
 using System;
+using System.Drawing;
 using RoboWarX;
 using RoboWarX.Arena;
 
 namespace RoboWarX.Weapons.Hellbore
 {
+    // Our small explosion type
+    internal class HellboreExplosion : BaseExplosion
+    {
+        public HellboreExplosion(Arena.Arena P, double X, double Y) : base(P, X, Y) { }
+        
+        public void onSpawn(Robot owner_, Robot target_)
+        {
+            base.baseOnSpawn(owner_, target_);
+        }
+        
+        protected override void impact()
+        {
+            target.shield = 0;
+        }
+    }
+    
+    // Hellbore projectile class
+    internal class HellboreObject : Projectile
+    {
+        public const bool offset = true;
+        public const bool collideProjectiles = false;
+
+        public HellboreObject(Arena.Arena P, double X, double Y) : base(P, X, Y) { }
+
+        public void onShoot(int energy_)
+        {
+            speedx = anglex * energy_ / 2.0;
+            speedy = angley * energy_ / 2.0;
+        }
+
+        public override void update()
+        {
+            // Hellbores update twice in a chronon
+            base.update();
+            // FIXME: do we need to check destruction here?
+            base.update();
+        }
+
+        public override bool onHit(ArenaObject other)
+        {
+            if (other != null)
+            {
+                if (!(other is Robot))
+                    return false;
+
+                owner.parent.spawn(typeof(HellboreExplosion), x, y, owner, other);
+            }
+            destroy();
+
+            return true;
+        }
+
+        public override void draw(System.Drawing.Graphics gfx)
+        {
+            gfx.DrawImage(Resources.Projectile.Hellbore, new Point((int)x - 4, (int)y - 4));
+        }
+    }
+    
     // Used to launch hellbores. Returns 0 if read, shoots hellbore with speed equal to amount
     // written (this is different than hellbores in RoboWar 2.1.2). This amount is removed from the
     // robot's energy supply. Hellbores reduce the shield of any robot they hit to zero but do no
     // other damage. They must move at a speed from 4 to 20 in the direction that the robot's
     // turret pointed when the shot was fired. Hellbores cannot be used unless they were first
     // enabled at the hardware store.
+    internal class HellboreRegister : Register
+    {
+        internal HellboreRegister() {
+            name = "HELLBORE";
+            code = (Int16)Bytecodes.REG_HELLBORE;
+        }
 
-    // FIXME: implement
+        public override Int16 value
+        {
+            get
+            {
+                return 0;
+            }
+
+            set
+            {
+                if (value < 4 || value > 20)
+                    return;
+                if (!robot.hardware.hasHellbores)
+                    throw new HardwareException(this.robot, "Hellbores not enabled.");
+                int power = robot.useEnergy(value);
+                if (power > 0)
+                    robot.shoot(typeof(HellboreObject), power);
+            }
+        }
+
+        public override Int16 param
+        {
+            set
+            {
+            }
+        }
+
+        public override Object Clone()
+        {
+            return new HellboreRegister();
+        }
+
+        public override bool checkInterrupt()
+        {
+            return false;
+        }
+
+        public override void updateInterruptState()
+        {
+        }
+    }
+
+    // Hellbore plugin entry
+    public class HellboreEntry : IPluginEntry
+    {
+        public ITemplateRegister[] getPrototypes()
+        {
+            HellboreRegister hellbore = new HellboreRegister();
+            return new ITemplateRegister[] { hellbore };
+        }
+    }
 }
