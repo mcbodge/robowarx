6683c181f39d19b04fa850673fa167011c908810
stephan@kochen.nl <stephan@kochen.nl@4cb8c14c-e4c9-11dd-9059-71fac19e68c6>
2009-01-31 17:39:21 +0000

    Split off a reusable BaseExplosion class from the Explosion class.

--- trunk/robowarx/LibRoboWarX/Arena/Explosion.cs
+++ trunk/robowarx/LibRoboWarX/Arena/Explosion.cs
@@ -1,46 +1,27 @@
 using System;
-using System.Drawing;
-using System.Collections.Generic;
-using System.Text;
 
 namespace RoboWarX.Arena
 {
     // Small explosion (used by non-explosive bullets)
-    public class Explosion : ArenaObject
+    public class Explosion : BaseExplosion
     {
-        private Robot owner;
-        private Robot target;
         private int damage;
-
+        
         public Explosion(Arena P, double X, double Y) : base(P, X, Y) { }
-
+        
         public void onSpawn(Robot owner_, Robot target_, int damage_)
         {
-            owner = owner_;
-            target = target_;
+            base.baseOnSpawn(owner_, target_);
             damage = damage_;
         }
-
-        public override void update()
+        
+        protected override void impact()
         {
-            base.update();
-
-            if (target != null)
-            {
-                bool damaged = target.doShotDamage(damage, owner);
-
-                if (damaged)
-                    target.hit = 2;
-                else if (target.hit == 0)
-                        target.hit = 1;
-            }
-
-            destroy();
-        }
-
-        public override void draw(Graphics gfx)
-        {
-            gfx.FillEllipse(Brushes.Black, new Rectangle((int)x - 5, (int)y - 5, 10, 10));
+            bool damaged = target.doShotDamage(damage, owner);
+            if (damaged)
+                target.hit = 2;
+            else if (target.hit == 0)
+                target.hit = 1;
         }
     }
 }
